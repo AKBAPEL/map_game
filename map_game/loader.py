a0326b96b46915b2602caba4dfbc3a45fa87f9aa
AKBAPEL <beldom2005@mail.ru>
2021-02-17 19:42:36 +0400

    Создали пустые функции для загрузчика.

---
+++ map_game/loader.py
@@ -0,0 +1,23 @@
+"""
+Загрузка файлов из XML файла
+
+"""
+import click
+
+
+@click.command()
+@click.argument('filename')
+def load(filenamee):
+    pass
+
+
+def parse():
+    pass
+
+
+def save(db):
+    pass
+
+
+if __name__ == "__main__":
+    load()
