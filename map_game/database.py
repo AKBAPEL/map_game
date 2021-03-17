f9f1e48155aeb395444789de063559b8dd5f54ea
AKBAPEL <beldom2005@mail.ru>
2021-03-17 19:31:38 +0400

    Добавлен скрипт для работы с Pygame

--- map_game/database.py
+++ map_game/database.py
@@ -22,7 +22,9 @@ class Database:
             pickle.dump(packed_data, f)
 
     def load(self):
-        ...
+        with open('data.pickle', 'rb') as f:
+            data_new = pickle.load(f)
+        return data_new
 
     def _pack(self, data: dict):
         self._make_points(data['nodes'])
