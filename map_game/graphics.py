dcfe75694b7f101e1973239373f1d7c59310f29a
Андрей <beldom2005@mail.ru>
2021-04-19 21:43:13 +0400

    Исправлено управление игроком

--- map_game/graphics.py
+++ map_game/graphics.py
@@ -1,64 +1,65 @@
 import pygame
+
 BLACK = (0, 0, 0)
 
 
 class Ground(pygame.sprite.Sprite):
-    def __init__(self,data,points):
+    def __init__(self, data, points):
         super().__init__()
         self.color = data.color
-        self.screen_coord = self.convert_coord(data,points)
-        pygame.sprite.Sprite.__init__(self)
-        dimension,min_coord = self.get_dimension(self.screen_coord)
+        self.screen_coord = self.convert_coord(data, points)
+        dimension, min_coord = self.get_dimension(self.screen_coord)
         self.image = pygame.Surface(dimension)
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.centerx = dimension[0]//2 + min_coord[0]
+        self.rect.centerx = dimension[0] // 2 + min_coord[0]
         self.rect.centery = dimension[1] // 2 + min_coord[1]
         self.shift_coordinates(*min_coord)
-
-    def shift_coordinates(self,x_min,y_min):
-        self.screen_coord = [(x-x_min, y - y_min)
-                             for (x,y) in self.screen_coord]
-
-    def convert_coord(self,data,points):
+        pass
+    def shift_coordinates(self, x_min, y_min):
+        self.screen_coord = [(x - x_min, y - y_min)
+                             for (x, y) in self.screen_coord]
+    def convert_coord(self, data, points):
         points_ = [points[p] for p in data.points]
         screen_coord = [(p.x, p.y) for p in points_]
-        return  screen_coord
-
+        return screen_coord
     def get_dimension(self, coords):
         x = [c[0] for c in coords]
         y = [c[1] for c in coords]
         diff_x = abs(max(x) - min(x))
         diff_y = abs(max(y) - min(y))
-        return (diff_x, diff_y),(min(x),min(y))
+        return (diff_x, diff_y), (min(x), min(y))
+
 
 class Polygon(Ground):
     def fill_surface(self):
         pygame.draw.polygon(self.image, self.color, self.screen_coord, 0)
-
 class Road(Ground):
     def __init__(self, data, points):
         super().__init__(data, points)
         self.width = data.width
-
     def fill_surface(self):
         pygame.draw.lines(self.image, self.color, False, self.screen_coord, self.width)
-
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.color = (255,255,255)
-        self.image = pygame.Surface((20,20))
+        self.color = (255, 255, 255)
+        self.image = pygame.Surface((20, 20))
         self.image.fill(self.color)
         self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
+        self.keys = {'up': False, 'down': False, 'left': False, 'right': False}
 
-    def update(self):
-        self.rect.centery += self.y
-
-    def move(self,keys):
-        self.x = 0
-        self.y = 0
-        if keys['down']:
-            self.y = 3
+    def move(self, keys_down, keys_up):
+        for d in ['down', 'up', 'left', 'right']:
+            if keys_down[d]:
+                self.keys[d] = True
+            if keys_up[d]:
+                self.keys[d] = False
+            if self.keys['down']:
+                self.rect.centery += 3
+            elif self.keys['up']:
+                self.rect.centery -= 3
+            elif self.keys['left']:
+                self.rect.centerx -= 3
+            elif self.keys['right']:
+                self.rect.centerx += 3
--- map_game/main.py
+++ map_game/main.py
@@ -4,76 +4,81 @@
 import pygame
 import map_game.database
 from map_game.graphics import Polygon, Road, Player
-
-
 def run():
-    WIDTH = 1024  # ширина игрового окна
-    HEIGHT = 768 # высота игрового окна
+    WIDTH = 800  # ширина игрового окна
+    HEIGHT = 600 # высота игрового окна
     FPS = 30 # частота кадров в секунду
     BLACK = (0, 0, 0)
-
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("My Game")
     clock = pygame.time.Clock()
-    player = Player()
-
     house_sprites, area_sprites, road_sprites = init_sprites()
-    #Цикл Игры
+    player = Player()
+    # Цикл игры
     running = True
     while running:
-        keys = {'up':False, 'down': False, 'left': False, 'right':False}
         clock.tick(FPS)
+        keys_down = {'up': False, 'down': False, 'left': False, 'right': False}
+        keys_up = {'up': False, 'down': False, 'left': False, 'right': False}
         for event in pygame.event.get():
             # проверить закрытие окна
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
-                    keys['down'] = True
-        # Рендеринг
-        screen.fill(BLACK)
-        area_sprites.draw(screen)
-        road_sprites.draw(screen)
-        house_sprites.draw(screen)
+                    keys_down['down'] = True
+                elif event.key == pygame.K_UP:
+                    keys_down['up'] = True
+                elif event.key == pygame.K_LEFT:
+                    keys_down['left'] = True
+                elif event.key == pygame.K_RIGHT:
+                    keys_down['right'] = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN:
+                    keys_up['down'] = True
+                elif event.key == pygame.K_UP:
+                    keys_up['up'] = True
+                elif event.key == pygame.K_LEFT:
+                    keys_up['left'] = True
+                elif event.key == pygame.K_RIGHT:
+                    keys_up['right'] = True
 
-        screen.blit(player.image,(10,10))
+        player.move(keys_down, keys_up)
+
+        # Рендеринг
+        screen.fill((100, 100, 100))
+        area_sprites.draw(screen)
+        house_sprites.draw(screen)
+        road_sprites.draw(screen)
+
+
+        screen.blit(player.image, player.rect.center)
         # после отрисовки всего, переворачиваем экран
         pygame.display.flip()
-
 
 def init_sprites():
     db = map_game.database.DataBase()
     data = db.load()
-    running = True
     points = data['Points']
     houses = data['Houses']
     areas = data['Areas']
     roads = data['Roads']
-    area_sprites = pygame.sprite.Group()
     house_sprites = pygame.sprite.Group()
+    area_sprites = pygame.sprite.Group()
     road_sprites = pygame.sprite.Group()
     for house in houses:
-        p = (Polygon(houses[house], points))
+        p = Polygon(houses[house], points)
         p.fill_surface()
         house_sprites.add(p)
     for area in areas:
-        p = (Polygon(areas[area], points))
+        p = Polygon(areas[area], points)
         p.fill_surface()
         area_sprites.add(p)
     for road in roads:
-        r = (Road(roads[road], points))
+        r = Road(roads[road], points)
         r.fill_surface()
         road_sprites.add(r)
-
     return house_sprites, area_sprites, road_sprites
-
-
-def convert_coord(house_data, points):
-    points_ = [points[p] for p in house_data.points]
-    screen_coord = [(p.x, p.y) for p in points_]
-    return screen_coord
-
-
 if __name__ == '__main__':
     run()
